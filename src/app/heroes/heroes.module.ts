44ce4bf8e4faa53b2bbf27303ca31cbf372c2252
Johan Bautista <jsbparra@gmail.com>
2023-05-28 23:49:47 +0200

    layout sidenav heroes

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -7,6 +7,7 @@ import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,6 @@ import { HeroPageComponent } from './pages/hero-page/hero-page.component';
     SearchPageComponent,
     HeroPageComponent,
   ],
-  imports: [CommonModule, HeroesRoutingModule],
+  imports: [CommonModule, HeroesRoutingModule, MaterialModule],
 })
 export class HeroesModule {}
